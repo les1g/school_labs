8d47fa2d40d41e308c3163ceb7a667f0eedd6ea5
Gisel Garrido <ggarr55@wgu.edu>
2025-01-28 22:06:08 -0700

    read file contents

--- File_Name_Change/file_name_change.py
+++ File_Name_Change/file_name_change.py
@@ -1,2 +1,6 @@
 # collect file name from user
-photo_file_names = input().strip() 
+photo_file_names = input().strip()
+
+# read the file contents
+with open(photo_file_names, 'r') as file_names:
+    file_names_list = file_names.readlines() # store file contents as a list

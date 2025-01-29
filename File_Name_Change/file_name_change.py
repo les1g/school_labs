c32da9bb4aa1e9c61ce863233d285163f9484573
Gisel Garrido <ggarr55@wgu.edu>
2025-01-28 22:19:45 -0700

    handle empty line in file

--- File_Name_Change/file_name_change.py
+++ File_Name_Change/file_name_change.py
@@ -9,9 +9,9 @@ with open(photo_file_names, 'r') as file_names:
 
 # modify file
 for name in file_names_list:
-    # handle edge case when file is empty
-    if name == '':
-        print('Nothing to modify.')
+    # handle edge case when file has empty new line
+    if name == '\n':
+        print('Nothing to modify.', end='')
     new_txt_file = name.replace('_photo.jpg', '_info.txt') # replace file name
     # put modified file name to output
     print(new_txt_file, end='')

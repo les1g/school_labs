75875373b8a20d8b21314ace28b15796f8647d88
Gisel Garrido <ggarr55@wgu.edu>
2025-01-28 22:10:35 -0700

    modify file and put to output

--- File_Name_Change/file_name_change.py
+++ File_Name_Change/file_name_change.py
@@ -4,3 +4,9 @@ photo_file_names = input().strip()
 # read the file contents
 with open(photo_file_names, 'r') as file_names:
     file_names_list = file_names.readlines() # store file contents as a list
+
+# modify file
+for name in file_names_list:
+    new_txt_file = name.replace('_photo.jpg', '_info.txt') # replace file name
+    # put modified file name to output
+    print(new_txt_file, end='')

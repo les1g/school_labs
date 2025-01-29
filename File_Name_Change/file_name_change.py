dfe48a485dcd16bb9fb6622b45d3a7c09620a336
Gisel Garrido <ggarr55@wgu.edu>
2025-01-28 22:14:36 -0700

    handle edge case - empty file

--- File_Name_Change/file_name_change.py
+++ File_Name_Change/file_name_change.py
@@ -7,6 +7,9 @@ with open(photo_file_names, 'r') as file_names:
 
 # modify file
 for name in file_names_list:
+    # handle edge case when file is empty
+    if name == '':
+        print('Nothing to modify.')
     new_txt_file = name.replace('_photo.jpg', '_info.txt') # replace file name
     # put modified file name to output
     print(new_txt_file, end='')
